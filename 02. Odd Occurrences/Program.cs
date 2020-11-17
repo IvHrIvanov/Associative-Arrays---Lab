ee3933f7107ef3dd3ac1d686135bd540a366451a
IvanHrIvanov <vinko96@abv.bg>
2020-11-18 01:23:23 +0200

    Add

---
+++ 02. Odd Occurrences/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _02._Odd_Occurrences
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] words = Console.ReadLine().ToLower().Split(" ");
+
+            Dictionary<string, int> savedWords = new Dictionary<string, int>();
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                string currentWord = words[i];
+                if (!savedWords.ContainsKey(currentWord))
+                {
+                    savedWords.Add(currentWord, 0);
+                }
+                savedWords[currentWord] += 1;
+            }
+
+            foreach (var item in savedWords)
+            {
+
+                if (item.Value % 2 == 1)
+                {
+                    Console.Write($"{item.Key} ");
+                }
+
+            }
+        }
+    }
+}
---
+++ 03. Word Synonyms/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03._Word_Synonyms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> synonyms = new Dictionary<string, List<string>>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+                string synonym = Console.ReadLine();
+
+                if(!synonyms.ContainsKey(input))
+                {
+                    synonyms.Add(input, new List<string>());
+                }
+                synonyms[input].Add(synonym);
+            }
+            foreach (var item in synonyms)
+            {
+                Console.WriteLine($"{item.Key} - {string.Join(", ", item.Value)}");
+                
+            }
+        }
+    }
+}
