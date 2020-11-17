0a396f281b95e6acdd4925bc0e48a11c34e43c58
IvanHrIvanov <vinko96@abv.bg>
2020-11-18 00:19:37 +0200

    Add project files.

---
+++ 01. Count Real Numbers/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Count_Real_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] input = Console.ReadLine().Split(" ")
+                                               .Select(double.Parse)
+                                               .ToArray();
+
+            Dictionary<double, int> numbers = new Dictionary<double, int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                double currentNumber = input[i];
+                if (!numbers.ContainsKey(input[i]))
+                {
+                    numbers.Add(currentNumber, 0);
+                }
+               
+                numbers[currentNumber] += 1;
+            }
+
+            foreach (var item in numbers.OrderBy(x=>x.Key))
+            {
+
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+
+            }
+        }
+    }
+}
