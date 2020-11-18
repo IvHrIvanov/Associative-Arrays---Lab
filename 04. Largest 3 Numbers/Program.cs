f75e716a43efe54c144f08d3cb361fece5c4ea0a
IvanHrIvanov <vinko96@abv.bg>
2020-11-18 02:05:49 +0200

    Add

---
+++ 04. Largest 3 Numbers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04._Largest_3_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split(" ")
+                .Select(int.Parse)
+                .ToList();
+
+            int[] numbersSort = numbers.OrderByDescending(n => n).ToArray();
+
+            if (numbersSort.Length < 3)
+            {
+                Console.WriteLine(string.Join(" ", numbersSort));
+            }
+            else
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    Console.Write(numbersSort[i] + " ");
+
+                }
+            }
+        }
+    }
+}
